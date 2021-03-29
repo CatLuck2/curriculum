fc5725586ffa56a90f3d4a03d4cbc6cfb180ae9e
猫吉 <freeaddress834@gmail.com>
2021-03-29 17:12:55 +0900

    Java2章Check2を提出

---
+++ JavaStudy/Check2/src/check/Check.java
@@ -0,0 +1,21 @@
+package check;
+
+public class Check {
+	
+	private static String firstName = "藤澤";
+	private static String lastName = "洋佑";
+	
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Pet pet = new Pet(constants.Constants.CHECK_CLASS_JAVA, constants.Constants.CHECK_CLASS_HOGE);
+		RobotPet robotPet = new RobotPet(constants.Constants.CHECK_CLASS_R2D2, constants.Constants.CHECK_CLASS_LUKE);
+		
+		printName(firstName, lastName);
+		pet.introduce();
+		robotPet.introduce();
+	}
+	
+	private static void printName(String first_name, String last_name) {
+		System.out.println("printNameメソッド → " + first_name + last_name);
+	}
+}
