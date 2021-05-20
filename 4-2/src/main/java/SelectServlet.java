272917cb33d5227c06ea14fce3b0b6204fe72dd8
CatLuck2 <freeaddress834@gmail.com>
2021-05-20 14:18:38 +0900

    処理の流れをコメントで記述、サーブレット名を変更

--- 4-2/src/main/java/SelectServlet.java
+++ 4-2/src/main/java/SelectServlet.java
@@ -12,6 +12,21 @@ import javax.servlet.http.HttpServletResponse;
  * 問①・②の回答をお願いします。
  *
  */
+/**
+ * 「処理の流れ」
+ * select.html
+ * 1：<form>で月（<select><option value="..">）を選択
+ * 2：送信（<input>）をクリック
+ * web.xml
+ * 3：SecretServlet.javaに紐づくurl-patternでURLを生成
+ * 4：URL（http://localhost:8080/4-2/url-pattern）をサーバーにリクエスト
+ * 5：サーバーでSelectServlet.javaが実行される
+ * SelectServlet.java
+ * 6：選択した月を、GET通信 - request.getParameter(引数)で文字列で取得
+ * 		引数には、select.html - <select>（月の選択肢を持つタグ）のname属性
+ * 7：月の出力結果をクライアント（Webブラウザ）に返す
+ * 
+ */
 public class SelectServlet extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response)
             throws IOException, ServletException {
